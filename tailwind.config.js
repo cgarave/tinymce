8ad707f02e70453e0b079607e8a982907731af45
Rave Flores <03100242.rave.f@031A-WEANB091.local>
2024-07-16 12:33:57 +0800

    changes 07162024

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
     extend: {},
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      'upper-roman': 'upper-roman',
+      'lower-roman': 'lower-roman',
+      'lower-alpha': 'lower-alpha',
+      'upper-alpha': 'upper-alpha',
+    }
   },
   plugins: [],
 }
